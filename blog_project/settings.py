a9d88b787a7509323881042ad16727cc608a553c
artur-at-work <arturatwork2018@gmail.com>
2019-03-07 01:58:18 -0500

    from blog_django server

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = [
    '192.168.1.110',
    'localhost',
    '127.0.0.1',
+   'artlab.hopto.org',
 ]
 
 
@@ -123,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
